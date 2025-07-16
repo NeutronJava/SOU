25e3f35e39f1a7b81b15de93e4bd1617b3477524
NeutronNerd <116937508+NeutronJava@users.noreply.github.com>
2025-07-15 22:47:33 -0500

    Create playsou.lua

---
+++ sample/playsou.lua
@@ -0,0 +1,24 @@
+-- Program to download and play sou on loop.
+-- code edited from https://github.com/Axisok/qtccv/blob/master/sample/playbadapple.lua
+local badapplerepository = "https://raw.githubusercontent.com/Axisok/qtccv/master/"
+local myrepository = "https://raw.githubusercontent.com/NeutronJava/SOU/master/"
+
+local function getFile(name, lname, repository)
+	local r = http.get(repository .. name, nil, true)
+	local f = fs.open(lname, "wb")
+	f.write(r.readAll())
+	f.close()
+	
+end
+
+if (not fs.exists("sou.qtv")) then
+	getFile("decode/apis/bitreader.lua", "apis/bitreader.lua", badapplerepository)
+	getFile("decode/apis/hexscreen.lua", "apis/hexscreen.lua", badapplerepository)
+	getFile("decode/apis/wave.lua", "apis/wave.lua", badapplerepository)
+	getFile("sample/sou2.lua", "sou2.lua", myrepository)
+	getFile("sample/sou.dfpwm", "sou.dfpwm", myrepository)
+	getFile("sample/sou.qtv", "sou.qtv", myrepository)
+	
+end
+
+shell.run("sou2", "souyeah")
